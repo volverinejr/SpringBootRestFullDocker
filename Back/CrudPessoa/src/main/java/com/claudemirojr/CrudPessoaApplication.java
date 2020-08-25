3cd20e7d698bc62668dbe980f9801cd35bc331eb
volverinejr <volverinejr@gmail.com>
2020-08-25 08:45:11 -0300

    Criando a estrutura inicial

---
+++ Back/CrudPessoa/src/main/java/com/claudemirojr/CrudPessoaApplication.java
@@ -0,0 +1,13 @@
+package com.claudemirojr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudPessoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudPessoaApplication.class, args);
+	}
+
+}
---
+++ Back/CrudPessoa/src/test/java/com/claudemirojr/CrudPessoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.claudemirojr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudPessoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
