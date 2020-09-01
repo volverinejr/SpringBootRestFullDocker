e65f249dbd99350a6a9d769c512fe3610687892d
volverinejr <volverinejr@gmail.com>
2020-09-01 10:32:52 -0300

    Ajustando o docker-compose

---
+++ Back/CrudPessoa/bin/src/main/resources/db/migration/V1__Create_Table_Pessoa.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `pessoas` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `endereco` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+)
